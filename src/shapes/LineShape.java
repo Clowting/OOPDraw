38116823c15765d71175e3a44710acffa5533036
Thijs Clowting <thijs@clowting.me>
2015-10-15 10:34:58 +0200

    Added files

---
+++ src/shapes/LineShape.java
@@ -0,0 +1,37 @@
+package shapes;
+
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+/**
+ * Creates and draws the line shape
+ */
+public class LineShape extends Shape {
+
+	private static final Color color = Color.RED;
+	private Line2D line;
+
+	public LineShape() {
+		line = new Line2D.Double();
+	}
+
+	@Override
+	public void setStartPoint(int x, int y) {
+		startPointX = x;
+		startPointY = y;
+	}
+
+	@Override
+	public void setEndPoint(int x, int y) {
+		endPointX = x;
+		endPointY = y;
+	}
+
+	@Override
+	public void Draw(Graphics2D g) {
+		line.setLine(startPointX, startPointY, endPointX, endPointY);
+
+		g.setColor(color);
+		g.draw(line);
+	}
+}
